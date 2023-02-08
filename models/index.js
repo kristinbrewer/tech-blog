40b968d0a15c656d0995aaea5d0a6ae9371acdc5
Kristin Brewer <kr527106@gmail.com>
2023-02-08 14:24:41 -0500

    updated FK paths in models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
 User.hasMany(Blog, {
   foreignKey: 'user_id',
@@ -10,4 +11,11 @@ Blog.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Blog };
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id'
+});
+
+module.exports = { User, Blog, Comment };
